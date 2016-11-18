4a8de6447b277fb74d4a17e2691ab745db7f5aba
Fabio Gomez <fabiogomez@us.ibm.com>
2016-11-18 15:27:21 -0600

    Formatting and new text

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource cabron');
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource cabron. Now test the automatic deploy');
 });
 
 module.exports = router;
